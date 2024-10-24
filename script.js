ba1d58535c76605e3df7fd3ac805ae30b0fd7a9b
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-24 20:45:00 -0300

    identificando a moeda

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const USD = 5.06
+const EUR = 5.10
+const GBP = 5.83
+
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
@@ -9,4 +13,19 @@ amount.addEventListener("input", () => {
 
 form.onsubmit = (event) => {
   event.preventDefault();
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
+}
+
+function convertCurrency (amount, price, symbol) {
+  console.log(amount, price, symbol); 
 }
