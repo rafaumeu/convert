8b05f6179e8c3614aff24669e33f80564ba11505
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-24 20:09:38 -0300

    recuperando a moeda escolhida pelo usuário

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 amount.addEventListener("input", () => {
   const hasCharctersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharctersRegex, "");
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+}
