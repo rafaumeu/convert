94d87649435314c8107372b5244325975f4f2dd6
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-24 20:54:56 -0300

    exibindo o footer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const GBP = 5.83
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 amount.addEventListener("input", () => {
   const hasCharctersRegex = /\D+/g
@@ -27,5 +28,11 @@ form.onsubmit = (event) => {
 }
 
 function convertCurrency (amount, price, symbol) {
-  console.log(amount, price, symbol); 
+  try {
+    footer.classList.add("show-result")
+  } catch (error) {
+    console.log(error)
+    footer.classList.remove("show-result")
+    alert("Não foi possível converter. Tente novamente mais tarde.")	
+  }
 }
